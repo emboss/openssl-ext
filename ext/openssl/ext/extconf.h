37946f2f59352f22f01b4cf835f0aebef99a88d8
emboss <Martin.Bosslet@googlemail.com>
2011-01-07 00:41:06 +0100

    Deleted unnecessary files.

--- ext/openssl/ext/extconf.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXTCONF_H
-#define EXTCONF_H
-#define HAVE_ASSERT_H 1
-#define HAVE_OPENSSL_SSL_H 1
-#define HAVE_RB_STR_SET_LEN 1
-#define HAVE_RB_BLOCK_CALL 1
-#define HAVE_OPENSSL_TS_H 1
-#endif
